c8d8f10e0d8cfe193d0e20968f6cc868958b9e48
nagatahiro <117128749+nagatahiro@users.noreply.github.com>
2024-12-18 08:15:26 +0900

    change wsgi.py

--- app_config/wsgi.py
+++ app_config/wsgi.py
@@ -20,12 +20,15 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
+print("FILE_PATH: ", os.path.dirname(__file__))
+print("PROJECT_NAME: ", os.path.basename(os.path.dirname(__file__)))
+
 FILE_PATH = os.path.dirname(__file__)
-PROJECT_NAME = os.path.basename(FILE_PATH)
+PROJECT_NAME = os.path.basename("~/waripoke.xyz/public_html/waripoke/app_config")
 
-sys.path.append(os.path.dirname(FILE_PATH))
-sys.path.append(FILE_PATH)
+sys.path.append(os.path.dirname("~/waripoke.xyz/public_html/waripoke/app_config"))
+sys.path.append("~/waripoke.xyz/public_html/waripoke/app_config")
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", PROJECT_NAME + ".settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app_config" + ".settings")
 
 application = get_wsgi_application()
