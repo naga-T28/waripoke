4cab5a77db936506c81956c07bef4ce4f8b2788c
nagatahiro <117128749+nagatahiro@users.noreply.github.com>
2024-12-19 14:58:33 +0900

    change wsgi

--- app_config/wsgi.py
+++ app_config/wsgi.py
@@ -7,29 +7,29 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app_config.settings')
-
-# application = get_wsgi_application()
-
 import os
 import sys
-
+from wsgiref.handlers import CGIHandler
 from django.core.wsgi import get_wsgi_application
 
+# 環境情報の出力 (デバッグ用)
 print("FILE_PATH: ", os.path.dirname(__file__))
 print("PROJECT_NAME: ", os.path.basename(os.path.dirname(__file__)))
 
+# プロジェクトのパスを設定
 FILE_PATH = os.path.dirname(__file__)
-PROJECT_NAME = os.path.basename("~/waripoke.xyz/public_html/app_config")
+PROJECT_PATH = os.path.expanduser("~/waripoke.xyz/public_html/app_config")
 
-sys.path.append(os.path.dirname("~/waripoke.xyz/public_html/app_config"))
-sys.path.append("~/waripoke.xyz/public_html/app_config")
+# プロジェクトのパスをsys.pathに追加
+sys.path.append(PROJECT_PATH)
+sys.path.append(os.path.dirname(PROJECT_PATH))
 
-
+# Djangoの設定を指定
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app_config.settings')
 
+# WSGIアプリケーションを取得
 application = get_wsgi_application()
+
+# CGIとして実行する場合のエントリポイント
+if __name__ == "__main__":
+    CGIHandler().run(application)
