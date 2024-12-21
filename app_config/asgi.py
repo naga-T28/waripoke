d351fb48ab1c9e1419ee205fa6c48c701fa3e181
nagatahiro <117128749+nagatahiro@users.noreply.github.com>
2024-12-21 17:11:11 +0900

    test3

--- app_config/asgi.py
+++ app_config/asgi.py
@@ -3,6 +3,7 @@ ASGI config for app_config project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
+
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
